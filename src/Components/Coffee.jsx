3915a4a62248eea1bfc9aeb0625ce3ccc78b5215
robbryandev <robbryandev@gmail.com>
2023-03-27 15:46:19 -0700

    Change coffee background display method

--- src/Components/Coffee.jsx
+++ src/Components/Coffee.jsx
@@ -7,7 +7,10 @@ export default function Coffee(props = {
 }) {
     return (
     <Card className=" bg-secondary p-4 m-4 rounded-lg shadow-md w-4/12 sm:w-3/12 md:w-1/5 lg:w-2/12 lg:max-w-[15vw]">
-        <Card.Img className="border-b-2 border-b-white pb-4" src={props.img} />
+        <div className="relative">
+            <Card.Img className="z-10 rounded-md aspect-square" src={props.img} />
+            <Card.Img className="rounded-sm z-20 absolute top-0 left-0 py-4" src="logo-mug.png" />
+        </div>
         <Card.Body>
             <Card.Title className="text-center text-xl text-white pt-2">{props.name}</Card.Title>
             <Card.Text className="text-lg text-center text-white">${props.price}</Card.Text>
--- src/menu.js
+++ src/menu.js
@@ -2,92 +2,12 @@ const menu = [
     {
         name: "black",
         price: 1.99,
-        img: "logo-mug.png"
+        img: "https://imgs.search.brave.com/gxot923L-gLExjk5E_meK5swdyxvP9OTPidoVxEkG5M/rs:fit:450:450:1/g:ce/aHR0cHM6Ly9tZWRp/YS5yYWlucG9zLmNv/bS9DaGVja2VyLzU2/MDc0LTkyOS5qcGc"
     },
     {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
-    },
-    {
-        name: "black",
-        price: 1.99,
-        img: "logo-mug.png"
+        name: "mocha",
+        price: 3.49,
+        img: "https://imgs.search.brave.com/ib5ICzQSyHrWfmnSJeO7ArdOd_mkwDcvZw8E-Co0VTU/rs:fit:713:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC45/dmNTUFdlSTU4cHN5/QVRWcjhCa0JnSGFF/NyZwaWQ9QXBp"
     }
 ]
 
