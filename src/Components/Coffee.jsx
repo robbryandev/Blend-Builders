5d24677d516d35d12dff9e01155c6ba919d5f5d4
robbryandev <robbryandev@gmail.com>
2023-03-28 10:31:04 -0700

    Add more flavors

--- src/Components/Coffee.jsx
+++ src/Components/Coffee.jsx
@@ -9,10 +9,10 @@ export default function Coffee(props = {
     <Card className=" bg-secondary p-4 m-4 rounded-lg shadow-md w-4/12 sm:w-3/12 md:w-1/5 lg:w-2/12 lg:max-w-[15vw]">
         <div className="relative">
             <Card.Img className="z-10 rounded-md aspect-square" src={props.img} />
-            <Card.Img className="rounded-sm z-20 absolute top-0 left-0 py-4" src="logo-mug.png" />
+            <Card.Img className="absolute top-0 left-0 z-20 py-4 rounded-sm" src="logo-mug.png" />
         </div>
         <Card.Body>
-            <Card.Title className="text-center text-xl text-white pt-2">{props.name}</Card.Title>
+            <Card.Title className="pt-2 text-center text-white text-md md:text-xl">{props.name}</Card.Title>
             <Card.Text className="text-lg text-center text-white">${props.price}</Card.Text>
         </Card.Body>
     </Card>
--- src/menu.js
+++ src/menu.js
@@ -8,6 +8,36 @@ const menu = [
         name: "Mocha",
         price: 3.49,
         img: "https://imgs.search.brave.com/ib5ICzQSyHrWfmnSJeO7ArdOd_mkwDcvZw8E-Co0VTU/rs:fit:713:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC45/dmNTUFdlSTU4cHN5/QVRWcjhCa0JnSGFF/NyZwaWQ9QXBp"
+    },
+    {
+        name: "Hazelnut",
+        price: 2.99,
+        img: "https://imgs.search.brave.com/lW8ypT46BPXpvlhv0zvtOJ_fNXehycTsZHw4eupikYw/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/bnV0cml0aW9uZXhw/cmVzcy5jb20vZmls/ZXMvYXNzZXRzL2lt/YWdlcy9zaHV0dGVy/c3RvY2tfMzc2Mzk3/Mzc3LmpwZw"
+    },
+    {
+        name: "Vanilla",
+        price: 2.99,
+        img: "https://imgs.search.brave.com/olqO7MlLAVAR8UpTRrdLfK6CLGh57uUvwGAxm3sYZ0Q/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9jZG4u/c2hvcGlmeS5jb20v/cy9maWxlcy8xLzA3/MDAvNzA0OS9wcm9k/dWN0cy9WYW5pbGxh/X0JlYW5zX1dob2xl/X01hZGFnYXNjYXJf/Qm91cmJvbl9CdWxr/X1dob2xlc2FsZV9C/dW5kbGUuanBnP3Y9/MTYwOTQzMDQ1MA"
+    },
+    {
+        name: "Latte",
+        price: 3.49,
+        img: "https://imgs.search.brave.com/M7gRV_2MHIeI4GpZtVI4IKLWbBWyDF7nVLbFLny23lE/rs:fit:632:225:1/g:ce/aHR0cHM6Ly90c2Uy/LmV4cGxpY2l0LmJp/bmcubmV0L3RoP2lk/PU9JUC4xZjdjSFM5/ZTVnMnlJekV2WWFt/UTBRSGFGaiZwaWQ9/QXBp"
+    },
+    {
+        name: "Irish",
+        price: 2.49,
+        img: "https://imgs.search.brave.com/tO8jCJFLhCskqE8wx5FXQt9CSWL2YPMuhW0eom6ydZY/rs:fit:225:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/ZERRVzZCUExUQ0d0/MTBNbFp1T193SGFI/YSZwaWQ9QXBp"
+    },
+    {
+        name: "Americano",
+        price: 1.99,
+        img: "https://imgs.search.brave.com/yLLA_LX-efBcEZh4Wi8SLCENEWpJdUMV_vtM6ksoVNw/rs:fit:1080:1080:1/g:ce/aHR0cHM6Ly9hYWtz/aC5jb20vd3AtY29u/dGVudC91cGxvYWRz/LzIwMTgvMDYvNi5q/cGc"
+    },
+    {
+        name: "Caramel",
+        price: 2.49,
+        img: "https://imgs.search.brave.com/H7ZbOlrT40vT8bDfJcbEtK-T-vXA_bFM43uG9aSZQNQ/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5k/WTh0R05MMXZWcnBX/cHVneFl1azBBSGFI/YSZwaWQ9QXBp"
     }
 ]
 
