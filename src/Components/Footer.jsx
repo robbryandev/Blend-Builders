7fb1edc02db6ffbf7f00ccd9ff3e635e1c839a6d
robbryandev <robbryandev@gmail.com>
2023-03-28 08:51:49 -0700

    Update contact section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,13 @@
 export default function Footer() {
   return (
-    <footer id="footer" className="w-full min-h-[20vh] bg-black">
-      <p className="text-white text-md p-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
-        reprehenderit repellat adipisci soluta quo cum deserunt voluptatem
-        corporis sapiente nobis fugit, delectus nulla necessitatibus molestiae
-        nihil culpa maiores numquam? Accusamus?
-      </p>
+    <footer id="footer" className="w-full min-h-[20vh] bg-black pb-12">
+      <h2 className="p-4 text-2xl text-center text-white md:text-3xl text-shadow">
+        Contact Us
+      </h2>
+      <p className="text-lg font-semibold text-center text-white">Email: <a className="font-normal text-md" href="mailto:robbryandev@gmail.com">robbryandev@gmail.com</a></p>
+      <p className="text-lg font-semibold text-center text-white">Phone: <span className="font-normal text-md">971-284-6154</span></p>
+      <p className="text-lg font-semibold text-center text-white">LinkedIn: <a className="font-normal text-md" href="https://www.linkedin.com/in/robbryandev/">linkedin.com/in/robbryandev</a></p>
+      <p className="text-lg font-semibold text-center text-white">Github: <a className="font-normal text-md" href="https://github.com/robbryandev">github.com/robbryandev</a></p>
     </footer>
   );
 }
