1786a7b1d9654824c726edc1a6e8360a68aebddb
robbryandev <robbryandev@gmail.com>
2023-03-28 08:27:39 -0700

    Replace media queries with event listener

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
+
 export default function Header() {
+  const isMobile = () => {
+    return window.innerWidth < 768
+    ? true
+    : false;
+  }
+  const [mobile, setMobile] = useState(isMobile());
+  useEffect(() => {
+    window.addEventListener("resize", () => setMobile(isMobile()));
+    return () => window.removeEventListener("resize", () => setMobile(isMobile()));
+  })
   return (
-    <header className="bg-primary border-b-4 border-b-secondary py-4">
-      <div id="logo" className="mx-2 logo">
-        <a href="/" className="py-3 pl-4">
+    <header className="py-4 border-b-4 bg-primary border-b-secondary">
+      <div id="logo" className="w-10 mx-2 md:w-44">
+        <a href="/">
+          <img className="w-10 md:w-44" src={mobile ? "logo-mug.png" : "logo-no-background.svg"} alt="blend builders logo" />
         </a>
       </div>
     </header>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,13 +10,13 @@ function Home() {
       <main>
         <div
           id="hero"
-          className="w-full h-[30vh] lg:h-[40vh] p-0 m-0 bg-hero pb-12"
+          className="w-full h-[30vh] md:h-[40vh] p-0 m-0 bg-hero pb-12"
         >
-            <h1 className="text-2xl md:text-4xl pl-4 pt-2 pb-6 text-white font-semibold text-shadow w-3/5">
+            <h1 className="w-3/5 pt-2 pb-6 pl-4 text-2xl font-semibold text-white md:text-4xl text-shadow">
               Welcome to Blend Builders, where every cup is a masterpiece!
             </h1>
             <a
-              className="text-white text-shadow text-xl md:text-2xl"
+              className="text-xl text-white text-shadow md:text-2xl"
               href="#footer"
             >
               <img
@@ -31,7 +31,7 @@ function Home() {
           id="menu"
           className="w-full min-h-[40vh] bg-primary border-y-secondary border-y-4"
         >
-          <h1 className="text-4xl text-white font-semibold text-center mt-4 text-shadow">
+          <h1 className="mt-4 text-4xl font-semibold text-center text-white text-shadow">
             Menu
           </h1>
           <div className="flex flex-row flex-wrap justify-center w-[80vw] m-auto my-4">
