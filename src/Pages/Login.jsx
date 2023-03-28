e5537a398b6795f8fd9f90c10c5d899b261d5908
robbryandev <robbryandev@gmail.com>
2023-03-28 14:24:41 -0700

    Add Demo details

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -7,7 +7,10 @@ function Login() {
       <Header showAuth={false} />
       <main className="w-screen min-h-screen pt-16 bg-black">
         <h1 className="mt-6 text-3xl text-center text-white text-shadow md:text-4xl">Admin Login</h1>
-        <div className="w-4/5 m-auto mt-8 md:max-w-[500px]">
+        <h3 className="pt-4 text-xl text-center text-white text-shadow">Demo Login</h3>
+        <p className="text-lg text-center text-white">Email: demo@example.com</p>
+        <p className="text-lg text-center text-white">Pass: 123456</p>
+        <div className="w-4/5 m-auto mt-8 md:max-w-[400px]">
           <LoginForm />
         </div>
       </main>
